f558a4e11458480491ff3649fda22e336e3689e3
Ashu <ashudhanda33@gmail.com>
2026-08-10 10:59:14 +0530

    Add factorial recursion example

---
+++ examples/FactorialRecursion.java
@@ -0,0 +1,11 @@
+// Factorial using recursion
+public class FactorialRecursion {
+    public static long factorial(int n) {
+        if (n <= 1) return 1;
+        return n * factorial(n - 1);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(factorial(5)); // 120
+    }
+}
