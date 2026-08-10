19ecf0e162c037b2382528c88f72a9180c05a332
Ashu <ashudhanda33@gmail.com>
2026-08-10 11:00:13 +0530

    Add count vowels example

---
+++ examples/CountVowels.java
@@ -0,0 +1,15 @@
+// Count vowels and consonants in a string
+public class CountVowels {
+    public static void main(String[] args) {
+        String s = "interview".toLowerCase();
+        int vowels = 0, consonants = 0;
+        for (char c : s.toCharArray()) {
+            if (c >= 'a' && c <= 'z') {
+                if ("aeiou".indexOf(c) >= 0) vowels++;
+                else consonants++;
+            }
+        }
+        System.out.println("Vowels: " + vowels + ", Consonants: " + consonants);
+        // Vowels: 4, Consonants: 5
+    }
+}
