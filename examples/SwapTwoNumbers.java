29ebf704a0508f6417d270c75a2acb1a63aa35f7
Ashu <ashudhanda33@gmail.com>
2026-08-10 10:59:59 +0530

    Add swap two numbers example

---
+++ examples/SwapTwoNumbers.java
@@ -0,0 +1,10 @@
+// Swap two numbers without a temp variable
+public class SwapTwoNumbers {
+    public static void main(String[] args) {
+        int a = 5, b = 10;
+        a = a + b;
+        b = a - b;
+        a = a - b;
+        System.out.println("a = " + a + ", b = " + b); // a = 10, b = 5
+    }
+}
