d2b94bf2eac941a7dcd32e88017a9763493e7b40
Ashu <ashudhanda33@gmail.com>
2026-08-10 10:58:32 +0530

    Add reverse string example

---
+++ examples/ReverseString.java
@@ -0,0 +1,10 @@
+// Reverse a string using StringBuilder
+public class ReverseString {
+    public static String reverse(String s) {
+        return new StringBuilder(s).reverse().toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(reverse("hello")); // olleh
+    }
+}
